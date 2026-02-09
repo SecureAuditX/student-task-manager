b7a54fccfc5374159c3821d2854c0b94bafaf7f4
SecureAuditX <abdulkarimumar86@gmail.com>
2026-02-09 16:32:03 +0800

    student logo in the sign up has been added

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUp from './components/Signup';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SignUp />
     </View>
   );
 }
@@ -15,6 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
