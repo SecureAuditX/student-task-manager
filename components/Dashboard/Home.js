50f132d0e042d53ad689d13ffdad04d72758091f
SecureAuditX <abdulkarimumar86@gmail.com>
2026-02-16 22:48:12 +0800

    I added add task button and Q&A button

--- components/Dashboard/Home.js
+++ components/Dashboard/Home.js
@@ -4,7 +4,8 @@ import {
     StyleSheet,
     Pressable,
     Platform,
-    Image
+    Image,
+    ScrollView
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
@@ -47,9 +48,43 @@ function HomeScreen(){
                 </Pressable>
             </View>
             
+            <ScrollView>
+            {/* Progress Bar Tracker Container */}
             <View>
                 <ProgressBar percent={65}/>
             </View>
+            
+            <View style={styles.taskSection}>
+                {/* Add Task Button */}
+            <Pressable style={styles.addTaskButton}>
+                <View>
+                    <Image 
+                    source={require("../../assets/add.png")}
+                    style={{width: 45, height: 45, resizeMode: "contain"}}
+                    />
+                </View>  
+                <Text style={styles.addTaskText}>Add Task</Text>  
+            </Pressable>
+
+                {/* Q&A Section */}
+            <Pressable style={styles.QAButton}>
+                <View>
+                    <Image 
+                    source={require("../../assets/Q&A.png")}
+                    style={{width: 35, height: 35, resizeMode: "contain"}}
+                    />
+                </View>  
+                <Text style={styles.QAText}>Q&A Section</Text>  
+            </Pressable>
+            </View>
+
+            {/* Tasks Section */}
+            <View>
+                <Text>Upcoming Tasks</Text>
+              
+            </View>
+            </ScrollView>
+
         </SafeAreaView>
     )
 }
@@ -97,6 +132,43 @@ const styles = StyleSheet.create({
         height: 30,
         justifyContent: "center",
         alignItems: "center"
+    },
+    taskSection: {
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        marginTop: 40,
+        alignItems: "center"
+    },
+    addTaskButton: {
+        backgroundColor: "#0F172A",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 170,
+        height: 120,
+        borderRadius: 20,
+        borderColor: "#030712",
+        marginRight: 20
+    },
+    addTaskText: {
+        fontSize: 14,
+        color: "#FFFFFF",
+        fontWeight: "bold",
+        marginTop: 10
+    },
+    QAButton: {
+        backgroundColor: "#FFFFFF",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 170,
+        height: 120,
+        borderRadius: 20,
+        borderColor: "#EDEDED",
+    },
+    QAText: {
+        fontSize: 14,
+        color: "#030712",
+        fontWeight: "bold",
+        marginTop: 10
     }
 })
 
