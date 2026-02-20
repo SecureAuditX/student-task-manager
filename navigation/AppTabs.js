7346f47cb098cceef7992da870d4b64d6ecdab4a
SecureAuditX <abdulkarimumar86@gmail.com>
2026-02-20 23:47:05 +0800

    I added a tab bar icons with active colors, label and style

--- navigation/AppTabs.js
+++ navigation/AppTabs.js
@@ -3,16 +3,53 @@ import HomeScreen from "../screens/home/Home";
 import StudySchedule from "../screens/home/StudySchedule";
 import Subject from "../screens/home/Subject";
 import Settings from "../screens/home/Settings";
+import {Image} from 'react-native'
 
 const Tab = createBottomTabNavigator()
 
 function AppTabs(){
     return (
-        <Tab.Navigator screenOptions={{headerShown: false}}>
-            <Tab.Screen name="Home"     component={HomeScreen} />
-            <Tab.Screen name="Schedule" component={StudySchedule} />
-            <Tab.Screen name="Subject"  component={Subject} />
-            <Tab.Screen name="Settings" component={Settings} />
+        <Tab.Navigator 
+        screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "#1E5BFF"
+        }}>
+            <Tab.Screen name="Home"     component={HomeScreen} 
+            options={{
+                tabBarIcon: ({ color, size }) => 
+                <Image source={require("../assets/home.png")}  
+                style={{width: size, height: size, tintColor: color}}
+                resizeMode="contain"
+                />
+            }}
+            />
+            <Tab.Screen name="Schedule" component={StudySchedule} 
+            options={{
+                tabBarIcon: ({ color, size}) => 
+                <Image source={require("../assets/appointment.png")}
+                style={{width: size, height: size, tintColor: color}}
+                resizeMode="contain"
+                />
+            }}
+            />
+            <Tab.Screen name="Subject"  component={Subject} 
+            options={{
+                tabBarIcon: ({ color, size }) =>
+                <Image source={require("../assets/book.png")}
+                style={{width: size, height: size, tintColor: color}}
+                resizeMode="contain"
+                />
+            }}
+            />
+            <Tab.Screen name="Settings" component={Settings} 
+            options={{
+                tabBarIcon: ({ color, size }) => 
+                <Image source={require("../assets/settings.png")}
+                style={{width: size, height: size, tintColor: color}}
+                resizeMode="contain"
+                />
+            }}
+            />
         </Tab.Navigator>
     )
 }
